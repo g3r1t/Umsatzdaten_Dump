0a6d4987715480dc8c594d4d195f4c06b2e581a0
g3r1t <gerit@posteo.de>
2022-11-29 16:12:19 +0100

    finally added fully boolean "Feiertage.csv"

--- Papierkorb/Feiertage_ics_import.R
+++ Papierkorb/Feiertage_ics_import.R
@@ -10,14 +10,15 @@ Bundesländer <- c("Baden-Wuerttemberg","Bayern","Berlin","Brandenburg","Bremen"
                   "Thueringen")
 
 #create dataframe with the right dimensions for the rbind later on
-feiertage <- data.frame(seq(as.Date("2013/1/1"), as.Date("2019/12/31"), by="day"))
-colnames(feiertage) <- "Datum"
+feiertage <- data.frame(matrix(ncol = 7, nrow = 0))
+
+#rename column names in order to be able to "rbind"
+colnames(feiertage) <- c("DTSTAMP", "UID", "DTSTART;VALUE=DATE", "DTEND;VALUE=DATE", "DESCRIPTION", 
+                      "SUMMARY", "Bundesland")
 
 dir.create("icals_feiertage")
 
 for (b in Bundesländer) {
-  dfb <- data.frame(matrix(ncol = 2, nrow = 0))
-  colnames(dfb) <- c("Datum", b)
   for (j in Jahre) {
     filename <- paste("Feiertage_",b,"_",j , sep = "")
     url <- paste(
@@ -25,31 +26,38 @@ for (b in Bundesländer) {
     filedest <- paste("icals_feiertage/", filename, sep = "")
     curl::curl_download(url, filedest)
     ical <- readLines(filedest)
-    dfj <- ic_dataframe(ical)
-    dfj[[b]] <- 1
-    colnames(dfj)[3] <- "Datum"
-    dfj <- dfj[,-c(1,2,4,5,6)]#drop redundant columns
-    dfb <- rbind(dfb, dfj)
+    df <- ic_dataframe(ical)
+    df$Bundesland <- b
+    feiertage <- rbind(feiertage, df)
   }
-  feiertage <- left_join(feiertage, dfb, by = "Datum")
 }
 
-#feiertage$Datum <- as.Date(feiertage$Datum)
+feiertage <- feiertage[,-c(1,2,4,5)]#drop redundant columns
+colnames(feiertage) <- c("Datum", "Feiertag", "Bundesland")
+feiertage$Datum <- as.Date(feiertage$Datum)
 
 #Import manually added data for 2013
 feiertage2013 <- read_delim("https://raw.githubusercontent.com/g3r1t/Umsatzdaten_Dump/main/Feiertage_2013.csv", 
                              delim = ";", escape_double = FALSE, trim_ws = TRUE,show_col_types = FALSE)
 
 feiertage2013$Datum <- as.Date(feiertage2013$Datum, "%d.%m.%Y")
+feiertage2013[is.na(feiertage2013)] <- 0
 
-feiertage2013 <- feiertage2013[,-c(1,3)]
+Extrawürste <- colnames(feiertage2013[,c(5:15)])
 
-feiertage2013 <- feiertage2013[c("Datum", Bundesländer)]
-
-feiertage <- rbind(feiertage, feiertage2013)
-
-feiertage <- feiertage[rowSums(is.na(feiertage)) != 16,]
-
-feiertage[is.na(feiertage)] <- 0
+for (b in Bundesländer){
+  if (b %in% Extrawürste){
+    df <- filter(feiertage2013, Alle == 1 | feiertage2013[[b]])
+    df$Bundesland <- b
+    df <- select(df, Datum, Feiertag, Bundesland)
+    feiertage <- rbind(feiertage, df)
+  }
+  else {
+    df <- filter(feiertage2013, Alle == 1)
+    df$Bundesland <- b
+    df <- select(df, Datum, Feiertag, Bundesland)
+    feiertage <- rbind(feiertage, df)
+  }
+}
 
 write.csv(feiertage,"Feiertage.csv", row.names = FALSE)
